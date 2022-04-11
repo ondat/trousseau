7825f93aabdacbb23c40936b669e83804525f088
rovandep <r.vandepoel@gmail.com>
2022-04-11 21:18:25 +0200

    fixing linting

--- internal/server/health.go
+++ internal/server/health.go
@@ -31,6 +31,7 @@ type HealthZ struct {
 func (h *HealthZ) Serve() error {
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc(h.HealthCheckURL.EscapedPath(), h.ServeHTTP)
+
 	if err := http.ListenAndServe(h.HealthCheckURL.Host, serveMux); err != nil && err != http.ErrServerClosed {
 		return fmt.Errorf("failed to start health check server: %w", err)
 	}
@@ -45,39 +46,49 @@ func (h *HealthZ) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer cancel()
 
 	conn, err := h.dialUnixSocket()
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 		return
 	}
+
 	defer conn.Close()
 
 	kmsClient := pb.NewKeyManagementServiceClient(conn)
 	err = h.checkRPC(ctx, kmsClient)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 		return
 	}
 
 	enc, err := h.Service.Encrypt(ctx, &pb.EncryptRequest{Plain: []byte(healthCheckPlainText)})
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	dec, err := h.Service.Decrypt(ctx, &pb.DecryptRequest{Cipher: enc.Cipher})
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	if string(dec.Plain) != healthCheckPlainText {
 		http.Error(w, "plain text mismatch after decryption", http.StatusInternalServerError)
 		return
 	}
+
 	w.WriteHeader(http.StatusOK)
 	_, err = w.Write([]byte("ok"))
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
 	klog.V(klogv).Infof("Completed health check")
 }
 
@@ -85,12 +96,15 @@ func (h *HealthZ) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // sends a KMS VersionRequest and checks if the VersionResponse is valid.
 func (h *HealthZ) checkRPC(ctx context.Context, client pb.KeyManagementServiceClient) error {
 	v, err := client.Version(ctx, &pb.VersionRequest{})
+
 	if err != nil {
 		return fmt.Errorf("unable to get version: %w", err)
 	}
+
 	if v.Version != version.APIVersion || v.RuntimeName != version.Runtime || v.RuntimeVersion != version.BuildVersion {
 		return errors.New("failed to get correct version response")
 	}
+
 	return nil
 }
 
