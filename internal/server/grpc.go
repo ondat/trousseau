525fba83c951a014e226216e9d297f582eb511fe
rovandep <r.vandepoel@gmail.com>
2022-04-11 20:11:38 +0200

    fix golangci-lint for internal/server/grpc.go

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -28,9 +28,8 @@ type keyManagementServiceServer struct {
 
 // New creates an instance of the KMS Service Server.
 func New(ctx context.Context, cfg config.ProviderConfig) (KeyManagementService, error) {
-	
 	kvClient, err := encrypt.NewService(cfg)
-	
+
 	if err != nil {
 		return nil, fmt.Errorf("unable to create encrypt service: %w", err)
 	}
@@ -48,29 +47,30 @@ func (k *keyManagementServiceServer) Decrypt(ctx context.Context, data *v1beta1.
 	defer func() {
 		errors := ""
 		status := metrics.SuccessStatusTypeValue
-		
+
 		if err != nil {
 			status = metrics.ErrorStatusTypeValue
 			errors = err.Error()
 		}
-		
+
 		k.reporter.ReportRequest(ctx, metrics.DecryptOperationTypeValue, status, time.Since(start).Seconds(), errors)
 	}()
 	klog.V(klogv).Infof("decrypt request started ")
-	
+
 	r, err := k.kvClient.Decrypt(data.Cipher)
 	if err != nil {
 		klog.ErrorS(err, "failed to decrypt")
 		return nil, fmt.Errorf("failed to decrypt: %w", err)
 	}
-	
+
 	w, err := base64.StdEncoding.DecodeString(string(r))
 	if err != nil {
 		klog.ErrorS(err, "failed decode encrypted data")
 		return nil, fmt.Errorf("failed decode encrypted data: %w", err)
 	}
-	
-	klog.V(2).Infof("decrypt request complete")
+
+	klog.V(klogv).Infof("decrypt request complete")
+
 	return &v1beta1.DecryptResponse{Plain: w}, nil
 }
 
@@ -80,21 +80,30 @@ func (k *keyManagementServiceServer) Encrypt(ctx context.Context, data *v1beta1.
 	var err error
 	defer func() {
 		errors := ""
+
 		status := metrics.SuccessStatusTypeValue
+
 		if err != nil {
 			status = metrics.ErrorStatusTypeValue
 			errors = err.Error()
 		}
+
 		k.reporter.ReportRequest(ctx, metrics.EncryptOperationTypeValue, status, time.Since(start).Seconds(), errors)
 	}()
+
 	klog.V(klogv).Infof("encrypt request started")
+
 	plain := base64.StdEncoding.EncodeToString(data.Plain)
+
 	response, err := k.kvClient.Encrypt([]byte(plain))
+
 	if err != nil {
 		klog.ErrorS(err, "failed to encrypt")
 		return nil, fmt.Errorf("failed to encrypt: %w", err)
 	}
-	klog.V(2).Infof("encrypt request complete")
+
+	klog.V(klogv).Infof("encrypt request complete")
+
 	return &v1beta1.EncryptResponse{Cipher: response}, nil
 }
 
